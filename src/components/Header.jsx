a62d9199ad2316dc274ef550b2ab1a4394b6ae6e
Makgomo-77 <102011950+Makgomo-77@users.noreply.github.com>
2025-10-19 10:54:15 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../components/AuthContext';
 
 const Header = () => {
   const { user, logout } = useAuth();
